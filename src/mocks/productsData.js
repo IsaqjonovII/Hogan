d103c2845b4459709fda70bb356b5e5c8b0ac79e
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-02-17 13:46:49 +0500

    products page is done

--- src/mocks/productsData.js
+++ src/mocks/productsData.js
@@ -3,54 +3,54 @@ export const productsData = [
     id: 0,
     title: "Sneakers H383 Blue",
     img_main:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-02.jpg?imwidth=248",
+      "	https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-02.jpg?imwidth=820",
     img_hover:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-03.jpg?imwidth=248",
+      "https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-03.jpg?imwidth=820",
     price: 990,
   },
   {
     id: 1,
     title: "Sneakers H383 Blue",
     img_main:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-02.jpg?imwidth=248",
+      "	https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-02.jpg?imwidth=820",
     img_hover:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-03.jpg?imwidth=248",
+      "https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-03.jpg?imwidth=820",
     price: 990,
   },
   {
     id: 2,
     title: "Sneakers H383 Blue",
     img_main:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-02.jpg?imwidth=248",
+      "	https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-02.jpg?imwidth=820",
     img_hover:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-03.jpg?imwidth=248",
+      "https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-03.jpg?imwidth=820",
     price: 990,
   },
   {
     id: 3,
     title: "Sneakers H383 Blue",
     img_main:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-02.jpg?imwidth=248",
+      "	https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-02.jpg?imwidth=820",
     img_hover:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-03.jpg?imwidth=248",
+      "https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-03.jpg?imwidth=820",
     price: 990,
   },
   {
     id: 4,
     title: "Sneakers H383 Blue",
     img_main:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-02.jpg?imwidth=248",
+      "	https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-02.jpg?imwidth=820",
     img_hover:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-03.jpg?imwidth=248",
+      "https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-03.jpg?imwidth=820",
     price: 990,
   },
   {
     id: 5,
     title: "Sneakers H383 Blue",
     img_main:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-02.jpg?imwidth=248",
+      "	https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-02.jpg?imwidth=820",
     img_hover:
-      "	https://www.hogan.com/fashion/hoganNew/HXM3830AN51QDW468G/HXM3830AN51QDW468G-03.jpg?imwidth=248",
+      "https://www.hogan.com/fashion/hoganNew/HXM5630EC01O6401A3/HXM5630EC01O6401A3-03.jpg?imwidth=820",
     price: 990,
   },
 ];
--- src/routes/Products/index.jsx
+++ src/routes/Products/index.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import { productsData } from "../../mocks/productsData.js"
-import "./style.css"
-
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import { productsData } from "../../mocks/productsData.js";
+import ProductCard from "../../components/Card";
+import "./style.css";
 
 const Products = () => {
-  console.log(productsData);
   return (
-    <div className='products__page__wrapper'>
-      
-    </div>
-  )
-}
+    <div className="products__page__wrapper">
+      <h1 className="page__title">Men's Sneakers</h1>
+      <div className="filter__wrapper flex">
+        <div className="filter flex">
+          <p className="filter__result">167 items found</p>
+          <div className="flex">
+            <p>filter by</p>
+            <BsChevronDown />
+          </div>
+        </div>
+        <p className="recommended__sort">
+          Sort by <b>Recommended</b> <BsChevronDown />
+        </p>
+      </div>
 
-export default Products
+      <div className="products__wrapper">
+        {productsData.map(({ id, title, img_main, img_hover, price }) => (
+          <ProductCard
+            key={id}
+            id={id}
+            title={title}
+            img_main={img_main}
+            img_hover={img_hover}
+            price={price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
